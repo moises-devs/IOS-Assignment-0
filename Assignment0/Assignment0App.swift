b2541bfbaef71277204a1849fc3d05610f8c0127
Moises <Mozlopez@ucdavis.edu>
2022-04-12 18:32:16 -0700

    Initial Commit

---
+++ Assignment0/Assignment0App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment0App.swift
+//  Assignment0
+//
+//  Created by Moises Lopez on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment0
+//
+//  Created by Moises Lopez on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
